c926f29e8234870a5b23ccaf9093dfdf82ff4d71
EmilioLombardo <emilomb3@gmail.com>
2025-07-02 21:52:05 +0200

    add TODO for disabling custom <c-e> and <c-y> keymaps in .md files

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,10 @@
 vim.wo.wrap = true
 vim.wo.linebreak = true
+
+-- TODO: Make my custom keymap for <c-e> and <c-y> not apply to markdown files,
+-- so they scroll only 1 instead of 2 lines.
+
+-- This here does not work, because the custom keymaps aren't defined by this
+-- point, so there is nothing to delete, and it produces and error.
+-- vim.keymap.del("n", "<c-e>", { buffer = 0 })
+-- vim.keymap.del("n", "<c-y>", { buffer = 0 })
