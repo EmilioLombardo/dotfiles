f9b8b0f8a351bf747e35b99fec0c7aad1bb27b49
EmilioLombardo <emilomb3@gmail.com>
2025-07-30 17:00:42 +0200

    edit desc of grapple keymaps, tweak <leader><leader>a keymap

--- neovim/.config/nvim/lua/plugins/other.lua
+++ neovim/.config/nvim/lua/plugins/other.lua
@@ -47,16 +47,16 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-      { "<leader><leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
-      { "<leader><leader>m", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+      { "<leader><leader>a", "<cmd>Grapple tag<cr>", desc = "Grapple: Tag a file" },
+      { "<leader><leader>m", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple: Toggle tags menu" },
 
-      { "<leader><leader>j", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-      { "<leader><leader>k", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-      { "<leader><leader>l", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-      { "<leader><leader>ø", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+      { "<leader><leader>j", "<cmd>Grapple select index=1<cr>", desc = "Grapple: Select first tag" },
+      { "<leader><leader>k", "<cmd>Grapple select index=2<cr>", desc = "Grapple: Select second tag" },
+      { "<leader><leader>l", "<cmd>Grapple select index=3<cr>", desc = "Grapple: Select third tag" },
+      { "<leader><leader>ø", "<cmd>Grapple select index=4<cr>", desc = "Grapple: Select fourth tag" },
 
-      { "<leader><leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
-      { "<leader><leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
+      { "<leader><leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple: Go to next tag" },
+      { "<leader><leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple: Go to previous tag" },
     },
     opts = {
       scope = "git",
