0905fcc1504bd5c740746fac996ca57217b64c3d
EmilioLombardo <emilomb3@gmail.com>
2025-08-27 18:33:14 +0200

    change lualine section separators again

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -94,8 +94,8 @@ return {
         component_separators = '│',-- '|',
         -- section_separators = '',
         -- section_separators = { left = '',right = '' },
-        -- section_separators = { left = ' ',right = ' ' },
-        section_separators = { left = ' ',right = '' },
+        section_separators = { left = '',right = '' },
+        -- section_separators = { left = ' ',right = '' },
       },
       sections = {
         lualine_b = { 'branch', 'diff', 'diagnostics', },
