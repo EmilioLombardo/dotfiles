a7e42027455b02479f7c50d8bcc7238b92c52b72
Emilio Lombardo <emilomb3@gmail.com>
2025-10-29 13:06:44 +0100

    add vim folds and edit comments

--- home.nix
+++ home.nix
@@ -1,11 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "emilio";
   home.homeDirectory = "/home/emilio";
 
+  # READ THIS BEFORE CHANGING home.stateVersion # {{{
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -13,34 +12,26 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
+  # }}}
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # --- Terminal utilities ---
+    neofetch tree fzf tmux zsh-powerlevel10k
 
-    neofetch
-    tree
-    fzf
-    tmux
+    # --- Hyprland ---
+    rofi waybar #dolphin
 
-    rofi
-    waybar
-
-    gcc
-
-    zsh-powerlevel10k
-
+    # --- LSP ---
     lua-language-server
     python313Packages.python-lsp-server
     nixd
     #clangd
 
-    # dolphin
+    # --- Other ---
+    gcc
 
+    # # Info: package overrides # {{{
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -53,22 +44,14 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    # # }}}
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".config/nvim".source = ./neovim/.config/nvim;
     ".tmux.conf".source = ./tmux/.tmux.conf;
     ".config/hypr".source = ./hyprland/.config/hypr;
     ".config/waybar".source = ./waybar/.config/waybar;
-    # ".p10k.zsh".source = ./powerlevel10k/.p10k.zsh;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   programs.git = {# {{{
@@ -80,20 +63,20 @@
     };
   };# }}}
 
-  programs.neovim = {
+  programs.neovim = {# {{{
     enable = true;
     defaultEditor = true;
     vimAlias = true;
-  };
+  };# }}}
 
-  programs.zsh = {
+  programs.zsh = { # {{{
     enable = true;
     enableCompletion = true;
     shellAliases = {
       q = "exit";
       d = "dirs -v | head -10";
     };
-    plugins = [
+    plugins = [# {{{
     {
       name = "powerlevel10k-config";
       src = ./powerlevel10k;
@@ -104,7 +87,7 @@
       src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
       file = "powerlevel10k.zsh-theme";
     }
-    {
+    { # Automatically use zsh when running nix-shell
       name = "zsh-nix-shell";
       file = "nix-shell.plugin.zsh";
       src = pkgs.fetchFromGitHub {
@@ -114,7 +97,7 @@
         sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
       };
     }
-    ];
+    ];# }}}
     initContent = ''
       # type only name of directory = cd into that directory (e.g. type .. to go up a dir)
       setopt autocd
@@ -127,17 +110,17 @@
       # zoxide: resolve symlinks before adding directories to the database
       export _ZO_RESOLVE_SYMLINKS=1
     '';
-  };
+  }; # }}}
 
-  programs.zoxide = {
+  programs.zoxide = {# {{{
     enable = true;
     enableZshIntegration = true;
     options = [
       "--cmd cd"
     ];
-  };
+  };# }}}
 
-  programs.tmux = {
+  programs.tmux = {# {{{
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "tmux-256color";
@@ -149,9 +132,9 @@
     extraConfig = ''
       set-option -g update-environment "PATH"
     '';
-  };
+  };# }}}
 
-  programs.ghostty = {
+  programs.ghostty = {# {{{
     enable = true;
     enableZshIntegration = true;
     settings = {
@@ -165,8 +148,10 @@
       adjust-box-thickness = 1;
       background-opacity = 0.8;
     };
-  };
+  };# }}}
 
+  # Info: home.sessionVariables # {{{
+  # 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
@@ -183,6 +168,7 @@
   #
   #  /etc/profiles/per-user/emilio/etc/profile.d/hm-session-vars.sh
   #
+  # }}}
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
@@ -191,4 +177,4 @@
   programs.home-manager.enable = true;
 }
 
-# vim: sts=2 sw=2
+# vim: sts=2 sw=2 foldmethod=marker foldmarker={{{,}}}
