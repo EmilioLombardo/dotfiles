ec69454271dc9eeb0eb09433ee9315e6f31f8fc3
Emilio Beier Lombardo <emiliobl@stud.ntnu.no>
2026-01-31 13:23:58 +0100

    separate treesitter-textobjects plugin spec from treesitter spec

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,13 +6,9 @@ return {
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
     event = "VeryLazy",
-    dependencies = {
-      -- Additional text objects via treesitter
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     config = function ()
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
+      require('nvim-treesitter.config').setup {
         -- Add languages to be installed here that you want installed for treesitter
         ensure_installed = {
           'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vimdoc', 'vim',
@@ -37,49 +33,66 @@ return {
             node_decremental = '<c-h>',
           },
         },-- }}}
-        textobjects = {
-          select = {-- {{{
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },-- }}}
-          move = { -- {{{
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },-- }}}
-          swap = {-- {{{
-            enable = true,
-            swap_next = { ['<leader>a'] = '@parameter.inner', },
-            swap_previous = { ['<leader>A'] = '@parameter.inner', },
-          },-- }}}
-        },
       }
     end
   },
+
+  { -- Treesitter-textobjects: additional textobjects {{{
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
+    main = "nvim-treesitter-textobjects",
+    opts = {
+      select = {-- {{{
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },-- }}}
+      move = { -- {{{
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },-- }}}
+      swap = {-- {{{
+        enable = true,
+        swap_next = { ['<leader>a'] = '@parameter.inner', },
+        swap_previous = { ['<leader>A'] = '@parameter.inner', },
+      },-- }}}
+    },
+  },-- }}}
 
 }
 
