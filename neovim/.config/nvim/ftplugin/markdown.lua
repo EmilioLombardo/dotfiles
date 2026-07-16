409186f5b4b34e9613d7750dddb24e31467d998d
EmilioLombardo <emilio.beier.lombardo@aenergi.no>
2026-07-16 09:42:13 +0200

    nvim: unset foldlevel
    Now all folds are open by default

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -2,5 +2,4 @@ vim.wo.wrap = false
 vim.wo.linebreak = true
 vim.wo.smoothscroll = true
 vim.wo.foldmethod = "expr"
-vim.wo.foldlevel = 30
 vim.wo.foldminlines = 1
--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -48,7 +48,6 @@ vim.opt.path:append("**") -- use :find to search in subdirectories as well
 
 -- FOLDS
 vim.o.foldmethod = "syntax"
-vim.o.foldlevelstart = 2
 vim.o.foldminlines = 3
 
 -- OTHER
